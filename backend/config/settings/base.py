9c0ea12bb66a24f1f3db32f290b388df6c387361
kenth charles repollo <kenthrepollo@gmail.com>
2026-05-08 15:16:13 +0800

    Fix database configuration to default to SQLite for local development

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -63,7 +63,7 @@ TEMPLATES = [
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ["DATABASE_URL"],
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR}/db.sqlite3"),
         conn_max_age=600,
         conn_health_checks=True,
     )
